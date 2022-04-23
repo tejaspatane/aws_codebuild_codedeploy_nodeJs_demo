a12be455c5e21ff6be7b189b417d540c25b4d1a7
tejas <tejas@gmail.com>
2022-04-23 12:01:05 +0530

    3rd see commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V5 for web hook!')
+  res.send('CICD App V2 see content!')
 })
 
 app.get('/status', (req, res) => {
